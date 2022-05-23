3890b8a8931474436f50e4350d56fc5ab18d1fee
David <depch47@gmail.com>
2022-05-23 18:14:40 -0500

    ADD---Tests

--- src/test/java/com/example/calculator/calculator/CalculatorTest.java
+++ src/test/java/com/example/calculator/calculator/CalculatorTest.java
@@ -16,20 +16,33 @@ class CalculatorTest {
 
     @Test
     void sumSuccessfully() {
-
+        assertEquals(10, underTest.sum(3, 3, 3, 1));
+        assertEquals(10, underTest.sum(5, 5));
+        assertEquals(60, underTest.sum(20, 20, 20));
     }
 
     @Test
     void subtractSuccessfully() {
-
+        assertEquals(5, underTest.subtract(10, 5));
+        assertEquals(0, underTest.subtract(10, 5, 5));
+        assertEquals(-5, underTest.subtract(5, 5, 5));
     }
 
     void divideSuccessfully() {
+        assertEquals(5, underTest.divide(10, 2));
+        assertEquals(10, underTest.divide(20, 2));
+        assertEquals(5, underTest.divide(100, 2, 2, 5));
+    }
 
+    void diveThrowsErrorIfThereIsZero() {
+        assertThrows(ArithmeticException.class, () -> underTest.divide(10, 0));
+        assertThrows(ArithmeticException.class, () -> underTest.divide(100, 2, 2, 0));
     }
 
     void multiplySuccessfully() {
-
+        assertEquals(10, underTest.divide(5, 2));
+        assertEquals(125, underTest.divide(5, 5, 5));
+        assertEquals(-100, underTest.divide(10, 5, -2));
     }
 
 
