4d4314e67247e605ec1482a8053a33837efe4fb7
David <depch47@gmail.com>
2022-05-23 15:09:12 -0500

    Calculator class

--- src/main/java/com/example/calculator/calculator/Calculator.java
+++ src/main/java/com/example/calculator/calculator/Calculator.java
@@ -1,27 +1,14 @@
 package com.example.calculator.calculator;
 
-import java.util.Arrays;
-
 public class Calculator {
 
-    public String sum(int ...nums) {
-        return String.valueOf(Arrays.stream(nums).sum());
+    public int sum(int x, int y) {
+        return x + y;
+    }
+
+    public int subtract(int x, int y) {
+        return x - y;
     }
 
 
-    public String subtract(int ...nums) {
-        return String.valueOf(Arrays.stream(nums).reduce((left, right) -> left - right).orElse(0));
-    }
-
-    public String divide(int ...nums) {
-        boolean anyMatch = Arrays.stream(nums).anyMatch(value -> value == 0);
-        if (anyMatch) {
-            return "Error";
-        }
-        return String.valueOf(Arrays.stream(nums).reduce((left, right) -> left / right).orElse(0));
-    }
-
-    public String multiply(int ...nums) {
-        return String.valueOf(Arrays.stream(nums).reduce((left, right) -> left * right).orElse(0));
-    }
 }
--- src/test/java/com/example/calculator/calculator/CalculatorTest.java
+++ src/test/java/com/example/calculator/calculator/CalculatorTest.java
@@ -16,15 +16,20 @@ class CalculatorTest {
 
     @Test
     void sumSuccessfully() {
-        assertEquals(String.valueOf(10), underTest.sum(5,5));
-        assertEquals(String.valueOf(60), underTest.sum(20, 10, 20, 10));
+
     }
 
     @Test
     void subtractSuccessfully() {
-        assertEquals(String.valueOf(5), underTest.subtract(10, 5));
-        assertEquals(String.valueOf(-5), underTest.subtract(5,5,5));
-        assertEquals(String.valueOf(40), underTest.subtract(50,5,5));
+
+    }
+
+    void divideSuccessfully() {
+
+    }
+
+    void multiplySuccessfully() {
+
     }
 
 
