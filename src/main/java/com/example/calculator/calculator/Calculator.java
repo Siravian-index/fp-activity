797c8d5b0e4a54624ce267795a507f52df8ffa29
David <depch47@gmail.com>
2022-05-23 18:48:25 -0500

    ADD---recursive sum

--- src/main/java/com/example/calculator/calculator/Calculator.java
+++ src/main/java/com/example/calculator/calculator/Calculator.java
@@ -1,20 +1,24 @@
 package com.example.calculator.calculator;
 
-public class Calculator  {
+public class Calculator {
 
-    public Integer sum(Integer ...args) {
+    public Integer sum(Integer x, Integer y) {
+        if (y == 0) {
+            return x;
+        } else {
+            return (1 + sum(x, y - 1));
+        }
+    }
+
+    public Integer subtract(Integer x, Integer y) {
         return 0;
     }
 
-    public Integer subtract(Integer ...args) {
+    public Integer multiply(Integer x, Integer y) {
         return 0;
     }
 
-    public Integer multiply(Integer ...args) {
-        return 0;
-    }
-
-    public Integer divide(Integer ...args) {
+    public Integer divide(Integer x, Integer y) {
         return 0;
     }
 }
--- src/test/java/com/example/calculator/calculator/CalculatorTest.java
+++ src/test/java/com/example/calculator/calculator/CalculatorTest.java
@@ -16,33 +16,34 @@ class CalculatorTest {
 
     @Test
     void sumSuccessfully() {
-        assertEquals(10, underTest.sum(3, 3, 3, 1));
-        assertEquals(10, underTest.sum(5, 5));
-        assertEquals(60, underTest.sum(20, 20, 20));
+        assertEquals(10, underTest.sum(4, 6));
+        assertEquals(33, underTest.sum(22, 11));
+        assertEquals(40, underTest.sum(20, 20));
     }
 
     @Test
     void subtractSuccessfully() {
         assertEquals(5, underTest.subtract(10, 5));
-        assertEquals(0, underTest.subtract(10, 5, 5));
-        assertEquals(-5, underTest.subtract(5, 5, 5));
+        assertEquals(0, underTest.subtract(5, 5));
+        assertEquals(-5, underTest.subtract(5, 10));
     }
 
+    @Test
     void divideSuccessfully() {
         assertEquals(5, underTest.divide(10, 2));
         assertEquals(10, underTest.divide(20, 2));
-        assertEquals(5, underTest.divide(100, 2, 2, 5));
     }
 
+    @Test
     void diveThrowsErrorIfThereIsZero() {
         assertThrows(ArithmeticException.class, () -> underTest.divide(10, 0));
-        assertThrows(ArithmeticException.class, () -> underTest.divide(100, 2, 2, 0));
     }
 
+    @Test
     void multiplySuccessfully() {
         assertEquals(10, underTest.divide(5, 2));
-        assertEquals(125, underTest.divide(5, 5, 5));
-        assertEquals(-100, underTest.divide(10, 5, -2));
+        assertEquals(25, underTest.divide(5, 5));
+        assertEquals(-100, underTest.divide(50, -2));
     }
 
 
